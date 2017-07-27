7d012537d26e7e0d8b0453a0ae353b25d2e44755
Enes <Moonstalker@users.noreply.github.com>
2017-07-27 19:20:45 +0300

    Update Yasakla.java

--- kaynak/effects/Yasakla.java
+++ kaynak/effects/Yasakla.java
@@ -1,17 +1,13 @@
 package effects;
 
-import org.apache.commons.io.IOExceptionWithCause;
 import org.bukkit.BanList.Type;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import javax.annotation.Nullable;
 
@@ -21,24 +17,25 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 
 public class Yasakla extends Effect {
-	private Expression<String> oyuncu;
-	private Expression<String> yazı1;
-	private Expression<String> yazı2; 
+	Expression<String> oyuncu;
+	Expression<String> yazı1;
+	Expression<String> yazı2; 
 	
 	@SuppressWarnings("unchecked")
-	@Override
-	  public boolean init(Expression<?>[] exp, int arg1, Kleenean arg2, ParseResult arg3) {
-		oyuncu = (Expression<String>) exp[0];
-		yazı1 = (Expression<String>) exp[1];
-		yazı2 = (Expression<String>) exp[2];
+	@Override public boolean init(Expression<?>[] exp, int arg1, Kleenean arg2, ParseResult arg3) {
+		oyuncu=(Expression<String>) exp[0];
+		yazı1=(Expression<String>) exp[1];
+		yazı2=(Expression<String>) exp[2];
 		return true;
-	  }
-	@Override
-	  public String toString(@Nullable Event evt, boolean arg1) {return null;}
-	@Override
-	  protected void execute(Event evt) {
+	}
+	@Override public String toString(@Nullable Event evt, boolean arg1) {return null;}
+	@Override protected void execute(Event a0) {
 		String sebep=null;
 		Date sonsüre=null;
+		int eklenecekyüzyıl=0;
+		int eklenecekyıl=0;
+		int eklenecekay=0;
+		int eklenecekhafta=0;
 		int eklenecekgün=0;
 		int ekleneceksaat=0;
 		int eklenecekdakika=0;
@@ -46,56 +43,74 @@ public class Yasakla extends Effect {
 		int sebepyazısı=0;
 		
 		if(yazı1!=null){
-			String yyazı1=yazı1.getSingle(evt).toString();
-			yyazı1.toLowerCase().replaceAll(Pattern.quote("\""),"");
+			String yyazı1=yazı1.getSingle(a0).toString().toLowerCase().replaceAll(Pattern.quote("\""),"");
+			if(yyazı1.contains("yüzyıl")){yyazı1=yyazı1.replaceAll("yüzyıl","").replaceAll(" ","");
+				try{eklenecekyüzyıl=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
+			}
+			if(yyazı1.contains("yıl")){yyazı1=yyazı1.replaceAll("yıl","").replaceAll(" ","");
+				try{eklenecekyıl=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
+			}
+			if(yyazı1.contains("ay")){yyazı1=yyazı1.replaceAll("ay","").replaceAll(" ","");
+				try{eklenecekay=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
+			}
+			if(yyazı1.contains("hafta")){yyazı1=yyazı1.replaceAll("hafta","").replaceAll(" ","");
+				try{eklenecekhafta=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
+			}
 			if(yyazı1.contains("gün")){yyazı1=yyazı1.replaceAll("gün","").replaceAll(" ","");
-				try{eklenecekgün=Integer.parseInt(yyazı1);sebepyazısı=2;}catch(NumberFormatException e){}
+				try{eklenecekgün=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
 			}
 			if(yyazı1.contains("saat")){yyazı1=yyazı1.replaceAll("saat","").replaceAll(" ","");
-				try{ekleneceksaat=Integer.parseInt(yyazı1);sebepyazısı=2;}catch(NumberFormatException e){}
+				try{ekleneceksaat=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
 			}
 			if(yyazı1.contains("dakika")){yyazı1=yyazı1.replaceAll("dakika","").replaceAll(" ","");
-				try{eklenecekdakika=Integer.parseInt(yyazı1);sebepyazısı=2;}catch(NumberFormatException e){}
+				try{eklenecekdakika=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
 			}
 			if(yyazı1.contains("saniye")){yyazı1=yyazı1.replaceAll("saniye","").replaceAll(" ","");
-				try{ekleneceksaniye=Integer.parseInt(yyazı1);sebepyazısı=2;}catch(NumberFormatException e){}
+				try{ekleneceksaniye=Integer.parseInt(yyazı1);sebepyazısı=1;}catch(Error e){}
 			}
 		}
 			
 		if(yazı2!= null){
-			String yyazı2=yazı2.getSingle(evt).toString();
-			yyazı2.toLowerCase().replaceAll(Pattern.quote("\""),"");
+			String yyazı2=yazı2.getSingle(a0).toString().toLowerCase().replaceAll(Pattern.quote("\""),"");
+			if(yyazı2.contains("yüzyıl")){yyazı2=yyazı2.replaceAll("yüzyıl","").replaceAll(" ","");
+				try{eklenecekyüzyıl=Integer.parseInt(yyazı2);}catch(Error e){}
+			}
+			if(yyazı2.contains("yıl")){yyazı2=yyazı2.replaceAll("yıl","").replaceAll(" ","");
+				try{eklenecekyıl=Integer.parseInt(yyazı2);}catch(Error e){}
+			}
+			if(yyazı2.contains("ay")){yyazı2=yyazı2.replaceAll("ay","").replaceAll(" ","");
+				try{eklenecekay=Integer.parseInt(yyazı2);}catch(Error e){}
+			}
+			if(yyazı2.contains("hafta")){yyazı2=yyazı2.replaceAll("hafta","").replaceAll(" ","");
+				try{eklenecekhafta=Integer.parseInt(yyazı2);}catch(Error e){}
+			}
 			if(yyazı2.contains("gün")){yyazı2=yyazı2.replaceAll("gün","").replaceAll(" ","");
-				try{eklenecekgün=Integer.parseInt(yyazı2);
-					sebepyazısı=1;}catch(NumberFormatException e){}
-				}
+				try{eklenecekgün=Integer.parseInt(yyazı2);}catch(Error e){}
+			}
 			if(yyazı2.contains("saat")){yyazı2=yyazı2.replaceAll("saat","").replaceAll(" ","");
-				try{ekleneceksaat=Integer.parseInt(yyazı2);
-					sebepyazısı=1;}catch(NumberFormatException e){}
+				try{ekleneceksaat=Integer.parseInt(yyazı2);}catch(Error e){}
 			}
 			if(yyazı2.contains("dakika")){yyazı2=yyazı2.replaceAll("dakika","").replaceAll(" ","");
-				try{eklenecekdakika=Integer.parseInt(yyazı2);
-					sebepyazısı=1;}catch(NumberFormatException e){}
+				try{eklenecekdakika=Integer.parseInt(yyazı2);}catch(Error e){}
 			}
 			if(yyazı2.contains("saniye")){yyazı2=yyazı2.replaceAll("saniye","").replaceAll(" ","");
-				try{ekleneceksaniye=Integer.parseInt(yyazı2);
-					sebepyazısı=1;}catch(NumberFormatException e){}
+				try{ekleneceksaniye=Integer.parseInt(yyazı2);}catch(Error e){}
 			}
 		}
 		
-		if(sebepyazısı==0){
-			sebep=yazı1.getSingle(evt).toString();
-		}
-		if(sebepyazısı==1){
-			sebep=yazı1.getSingle(evt).toString();
-		}
-		if(sebepyazısı==2){
-			sebep=yazı2.getSingle(evt).toString();
+		if(yazı1!=null){sebep=yazı1.getSingle(a0).toString();
+			if(sebepyazısı==1&&yazı2!=null){sebep=yazı2.getSingle(a0).toString();}
 		}
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Calendar calendar = new GregorianCalendar();
 		
+		if(eklenecekyüzyıl>0){eklenecekyüzyıl=eklenecekyüzyıl*100;}
+		
+		if(eklenecekyüzyıl>0){calendar.add(Calendar.YEAR, eklenecekyüzyıl);}
+		if(eklenecekyıl>0){calendar.add(Calendar.YEAR, eklenecekyıl);}
+		if(eklenecekay>0){calendar.add(Calendar.MONTH, eklenecekay);}
+		if(eklenecekhafta>0){calendar.add(Calendar.WEEK_OF_MONTH, eklenecekhafta);}
 		if(eklenecekgün>0){calendar.add(Calendar.DAY_OF_MONTH, eklenecekgün);}
 		if(ekleneceksaat>0){calendar.add(Calendar.HOUR, ekleneceksaat);}
 		if(eklenecekdakika>0){calendar.add(Calendar.MINUTE, ekleneceksaat);}
@@ -104,7 +119,7 @@ public class Yasakla extends Effect {
 
         sonsüre=calendar.getTime();
 
-		Bukkit.getBanList(Type.NAME).addBan(oyuncu.getSingle(evt).toString().replaceAll(Pattern.quote("\""),""), sebep, sonsüre, null);
+		Bukkit.getBanList(Type.NAME).addBan(oyuncu.getSingle(a0).toString().replaceAll(Pattern.quote("\""),""), sebep, sonsüre, null);
 		
 	  }
 }
