e31afbe265ea552d55372ec5efae0f49612d33e1
Enes <Moonstalker@users.noreply.github.com>
2017-07-29 12:02:49 +0300

    Daha fazla Event ve Effect eklendi.

--- kaynak/kaydet/Kayıt.java
+++ kaynak/kaydet/Kayıt.java
@@ -1,10 +1,17 @@
 package kaydet;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.util.SimpleEvent;
+import effects.KonsolaMesajGönder;
+import effects.OyuncuyaMesajGönder;
 import effects.SunucuKapat;
 import effects.Yasakla;
 import expressions.oplist;
@@ -21,7 +28,20 @@ public class Kayıt extends JavaPlugin {
             		"(ban|banla|yasakla) -> %string% [-> [(sebep|neden|süre|zaman):] %-string% [boyunca]] [-> [(sebep|neden|süre|zaman):] %-string% [boyunca]]",
             		"%string% -> (ban|banla|yasakla) [-> [(sebep|neden|süre|zaman):] %-string% [boyunca]] [-> [(sebep|neden|süre|zaman):] %-string% [boyunca]]",
             		"%string% [-> [(sebep|neden|süre|zaman):] %-string% [boyunca] [(banla|yasakla)]] [-> [(sebep|neden|süre|zaman):] %-string% [boyunca] [(banla|yasakla)]]");
-        }else{setEnabled(false);}
+            Skript.registerEffect(OyuncuyaMesajGönder.class,
+            		"mesaj gönder -> %player% -> %string%");
+            Skript.registerEffect(KonsolaMesajGönder.class,
+            		"konsola mesaj gönder -> %string%");
+            
+            registerEvent(PlayerJoinEvent.class, "[sunucuya] (girildiğinde|giriş yapıldığında)");
+            registerEvent(PlayerQuitEvent.class, "[sunucudan] (çıkış yapıldığında|çıkıldığında)");
+            registerEvent(BlockDamageEvent.class, "blok kırılmaya başlandığında");
+            registerEvent(PlayerMoveEvent.class, "herhangi bir yürümede");
+		
+		}else{setEnabled(false);}
 	}
-
+	@SuppressWarnings("unchecked")
+	public static void registerEvent(@SuppressWarnings("rawtypes") Class clazz, String syntax) {
+		Skript.registerEvent(syntax, SimpleEvent.class, clazz, syntax);
+	}
 }
