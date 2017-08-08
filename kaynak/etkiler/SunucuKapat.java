b646911be6c8f24285cb7d8fba34d53c238645b5
Enes <Moonstalker@users.noreply.github.com>
2017-08-08 15:21:36 +0300

    Hatalar düzeltildi.

---
+++ kaynak/etkiler/SunucuKapat.java
@@ -0,0 +1,50 @@
+/**
+ *  
+ *  This file is part of SkripTR. SkripTR is free software: you can redistribute it and/or modify
+ *  SkripTR is using Skript software.
+ * 
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright 2011-2017 Peter GÃ¼ttinger and contributors
+ */
+
+package etkiler;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import javax.annotation.Nullable;
+
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+public class SunucuKapat extends Effect {
+
+	@Override
+	public boolean init(Expression<?>[] arg0, int arg1, Kleenean arg2, ParseResult arg3) {
+		return true;
+	}
+
+	@Override
+	public String toString(@Nullable Event arg0, boolean arg1) {
+		return "";
+	}
+
+	@Override
+	protected void execute(Event arg0) {
+		Bukkit.getServer().shutdown();
+	}
+}
